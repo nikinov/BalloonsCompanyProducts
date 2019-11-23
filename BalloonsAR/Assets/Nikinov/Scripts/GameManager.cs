1b539c265c6889569b26b883ca917f16a145280f
nikinov <novellenicholas@gmail.com>
2019-11-23 20:20:13 +0100

    GameManager

---
+++ BalloonsAR/Assets/Nikinov/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Stuf used in Multiple classes
+
+    //Lists
+    public List<GameObject> BalloonArrays;
+    public List<GameObject> Strings;
+    public List<GameObject> ColourVariance;
+    public List<GameObject> Products;
+
+    //ints
+    //Sel = Selected
+    public int SelBallArray;
+    public int SelStringArray;
+    public int SelColourVariant;
+    public int SelProduct;
+
+    //functions
+    public void AddBall ()
+    {
+
+    }
+    public void AddString ()
+    {
+
+    }
+    public void AddColour ()
+    {
+
+    }
+    public void AddProduct ()
+    {
+
+    }
+}
