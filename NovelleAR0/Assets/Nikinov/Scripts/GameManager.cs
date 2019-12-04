653c9d3fa32133e8dbc9761735504adf48d452d2
nikinov <novellenicholas@gmail.com>
2019-12-04 21:04:20 +0100

    blablblbla

---
+++ NovelleAR0/Assets/Nikinov/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject obj0;
+    public GameObject obj1;
+
+    public GameObject colourUI1;
+
+    public GameObject obj0UI;
+    public GameObject obj1UI;
+
+    private void Awake()
+    {
+        obj0.SetActive(false);
+        obj1.SetActive(false);
+
+        obj0UI.SetActive(false);
+        obj1UI.SetActive(false);
+    }
+
+    public void SetA ()
+    {
+        obj0.SetActive(true);
+        obj1.SetActive(false);
+
+        obj0UI.SetActive(true);
+        obj1UI.SetActive(false);
+    }
+    public void SetB()
+    {
+        obj0.SetActive(false);
+        obj1.SetActive(true);
+
+        obj0UI.SetActive(false);
+        obj1UI.SetActive(true);
+    }
+}
---
+++ NovelleAR0/Assets/Nikinov/Strings/SlideManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideManager : MonoBehaviour
+{
+    public float timer;
+    public Slider slider0;
+    public Slider slider1;
+    public bool chek;
+
+    private void FixedUpdate()
+    {
+        Debug.Log("slid" + slider0.value);
+        if (!chek)
+        {
+            if (slider0.value <= .5f)
+            {
+                if (slider0.value > 0)
+                {
+                    slider0.value -= .5f;
+                }
+            }
+            if (slider0.value > .5f)
+            {
+                if (timer < 20)
+                {
+                    slider0.value += .5f;
+                }
+            }
+            if (slider1.value <= .5f)
+            {
+                if (slider1.value > 0)
+                {
+                    slider1.value -= .5f;
+                }
+            }
+            if (slider1.value > .5f)
+            {
+                if (timer < 20)
+                {
+                    slider1.value += .5f;
+                }
+            }
+        }
+        if (timer > 0)
+        {
+            timer -= 70 * Time.deltaTime;
+        }
+        if (timer <= 0.1)
+        {
+            chek = false;
+        }
+        if (timer > 20)
+        {
+            timer = 20;
+        }
+    }
+    public void BringUpMenu0 (float val)
+    {
+        chek = true;
+        timer += 10;
+    }
+    public void BringUpMenu1 (float val)
+    {
+        chek = true;
+        timer += 10;
+    }
+}
