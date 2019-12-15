6c43d4fd132e781ba4d3c22e373512b0e76948f3
nikinov <novellenicholas@gmail.com>
2019-12-15 17:10:06 +0100

    ...

---
+++ NovelleAR0/Assets/Nikinov/Scripts/SelectDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDetection : MonoBehaviour
+{
+    void Update()
+    {
+        Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == (""))
+            {
+
+            }
+        }
+    }
+}
