9625eb7a35ba428cebf33549f2476762f4a74b9d
nikinov <novellenicholas@gmail.com>
2019-12-29 14:23:21 +0100

    how to use video

--- NovelleAR0/Assets/newStuf/Scripts/MainMenuManager.cs
+++ NovelleAR0/Assets/newStuf/Scripts/MainMenuManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -28,6 +30,7 @@ public class MainMenuManager : MonoBehaviour
     public void BackToMainPanel()
     {
         animator.SetInteger("Panels", 0);
+        Pause();
     }
     public void Unlock()
     {
@@ -44,9 +47,49 @@ public class MainMenuManager : MonoBehaviour
         //{
         //    Unlock();
         //}
+
+        pause.SetActive(false);
     }
     public void GetRidOfUnlock()
     {
         PlayerPrefs.SetInt("unlock", 0);
+    }
+
+    //video
+    public RawImage image;
+    public RawImage image2;
+    public VideoPlayer vid;
+    public AudioSource audio;
+    public GameObject PlayButton;
+    public GameObject pause;
+
+    public void PlayVideo()
+    {
+        StartCoroutine(PlayVid());
+        PlayButton.SetActive(false);
+
+    }
+    IEnumerator PlayVid()
+    {
+        vid.Prepare();
+        WaitForSeconds wait = new WaitForSeconds(1);
+        while (!vid.isPrepared)
+        {
+            yield return wait;
+            break;
+        }
+        image.texture = vid.texture;
+        vid.Play();
+        audio.Play();
+        pause.SetActive(true);
+    }
+    public void Pause()
+    {
+        PlayButton.SetActive(true);
+        vid.Stop();
+        audio.Stop();
+        pause.SetActive(false);
+        PlayButton.SetActive(true);
+        image.texture = image2.texture;
     }
 }
